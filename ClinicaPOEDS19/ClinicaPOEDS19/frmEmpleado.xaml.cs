494fc501856f16f13b97e8ec7676d3c9e8ca1021
hector97xd <37050929+hector97xd@users.noreply.github.com>
2021-11-02 15:45:15 -0600

    #3
    formulario doctor

---
+++ ClinicaPOEDS19/ClinicaPOEDS19/frmEmpleado.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ClinicaPOEDS19.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClinicaPOEDS19
+{
+    /// <summary>
+    /// Lógica de interacción para frmEmpleado.xaml
+    /// </summary>
+    public partial class frmEmpleado : Window
+    {
+        DaoEmpleado daoempleado = new DaoEmpleado();
+        public frmEmpleado()
+        {
+            InitializeComponent();
+            dgEmpleado.ItemsSource = daoempleado.GetAll();
+        }
+    }
+}
