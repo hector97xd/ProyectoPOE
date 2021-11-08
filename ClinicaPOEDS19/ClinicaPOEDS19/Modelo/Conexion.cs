f7214e0e8c24828bf7ba9eea0cb71abb4167526b
guillerz <guillermo1897@gmail.com>
2021-11-08 00:54:57 -0600

    test

--- ClinicaPOEDS19/ClinicaPOEDS19/Modelo/Conexion.cs
+++ ClinicaPOEDS19/ClinicaPOEDS19/Modelo/Conexion.cs
@@ -25,40 +25,6 @@ namespace ClinicaPOEDS19.Modelo
             }
         }
 
-     /*
-        public void Add(Product prod)
-        {
-            try
-            {
-                using (IDbConnection cn = GetConnection)
-                {
-                    var query = @"INSERT INTO PRODUCT(Name,Quantity,Price) VALUES(@Name,@Quantity,@Price)";
-                    cn.Open();
-                    cn.Execute(query, prod);
-                }
-            }
-            catch (Exception ex)
-            {
-                ex.ToString();
-            }
-        }
-        public void Delete(int id)
-        {
-            using (IDbConnection cn = GetConnection)
-            {
-                var query = @"DELETE FROM PRODUCT WHERE ProductId=@Id";
-                cn.Open();
-                cn.Execute(query, new { Id = id });
-            }
-        }
-        public void Update(Product prod)
-        {
-            using (IDbConnection cn = GetConnection)
-            {
-                var query = @"UPDATE PRODUCT SET Name=@Name,Quantity=@Quantity,Price=@Price WHERE ProductId=@ProductId";
-                cn.Open();
-                cn.Execute(query, prod);
-            }
-        }*/
+     
     }
 }
